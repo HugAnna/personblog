44a2748e937c6367dac650e28eef9028fda6624b
路明泽 <2250206701@qq>
2024-02-07 15:08:39 +0800

    pwa等修改

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,20 @@
 module.exports = {
+    base: "/personblog/",
+    title: '鸢飞戾天者望峰息心',
+    description:'一个简单的个人博客',
+    head: [
+        ['link', { rel: 'icon', href: '/favicon.icon' }],
+        ['meta', { name: 'author',content:'望峰息心' }],
+        ['meta', { name: 'keywords',content:'vuepress介绍,说明,望峰息心' }],
+        ['link', { rel: 'manifest', href: '/manifest.json' }],
+        ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+        ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+        ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+        ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+        ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+    ],
     themeConfig: {
         logo: '/assets/img/test4.jpg',
         sidebar: 'auto',
@@ -31,6 +47,25 @@ module.exports = {
             }
         ]
     },
- 
+    plugins: [
+        [
+            '@vuepress/last-updated'
+    ],
+    ['@vuepress/pwa', {
+            serviceWorker: true,
+            updatePopup: {
+                '/': {
+                    message: "New content is available.",
+                    buttonText: "Refresh"
+                },
+                '/zh/': {
+                    message: "发现新内容可用",
+                    buttonText: "刷新"
+                }
+            }
+        }
+        ]
+        
+    ]
     
   }
